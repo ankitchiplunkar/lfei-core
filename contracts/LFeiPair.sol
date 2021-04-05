55d6e8931d7c549afdb53ac28d25e87d967c62ef
Ankit Chiplunkar <ankitchiplunkar@gmail.com>
2021-04-05 20:58:00 +0100

    initial commit

---
+++ contracts/LFeiPair.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@uniswap/lib/contracts/libraries/TransferHelper.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+/**
+ * @notice A mintable ERC20
+ */
+contract LFeiPair is ERC20 {
+    using SafeMath for uint256;
+
+    address public usdc;
+    address public fei;
+    uint128 constant usdcOutNumerator = 95; // fee is 0.5%
+    uint128 constant denominator = 1000; // fee is 0.5%
+
+    address contractCreator;
+    uint256 public conversionRateNumerator; // this conversion rate is divided by denominator (1000)
+
+    constructor(
+        uint256 _conversionRateNumerator,
+        address _fei,
+        address _usdc
+    ) public ERC20("LFeiPair", "LFP") {
+        conversionRateNumerator = _conversionRateNumerator;
+        contractCreator = msg.sender;
+        fei = _fei;
+        usdc = _usdc;
+    }
+
+    receive() external payable {}
+
+    // Deposit Fei into the contract and mint equivalent amount of LFeiPair tokens
+    function depositFei(uint256 amountFeiIn) public {
+        TransferHelper.safeTransferFrom(fei, msg.sender, address(this), amountFeiIn);
+        _mint(msg.sender, amountFeiIn);
+    }
+
+    // Burn LFeiPair from the sender and send equivalent amount of Fei tokens
+    function withdrawFei(uint256 amountLFeiIn) public {
+        _burn(msg.sender, amountLFeiIn);
+        TransferHelper.safeTransferFrom(fei, address(this), msg.sender, amountLFeiIn);
+    }
+
+    // Burn LFeiPair from the sender and send equivalent amount of Fei tokens
+    function withdrawUSDC(uint256 amountLFeiIn) public {
+        _burn(msg.sender, amountLFeiIn);
+        uint256 amountUSDCOut = amountLFeiIn.mul(usdcOutNumerator).div(denominator);
+        uint256 amountUSDCWithdrawn = amountUSDCOut.mul(conversionRateNumerator).div(denominator);
+        uint256 amountUSDCFees = amountUSDCOut.sub(amountUSDCWithdrawn);
+        TransferHelper.safeTransferFrom(usdc, address(this), msg.sender, amountUSDCWithdrawn);
+        TransferHelper.safeTransferFrom(usdc, address(this), contractCreator, amountUSDCFees);
+    }
+}
